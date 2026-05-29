4fce5ead80cc939e69b70a39167ab7bf97e2ce33
ayuwooh <ayuwooh@gmail.com>
2026-05-29 13:06:35 -0300

    removed files

--- PA — Programação e Algoritmos/Atividades/08-pratica-repeticao/1---tabuada-do-15.py
+++
@@ -1,8 +0,0 @@
-number,result=0,0
-
-while True:
-    result=15*number
-    print(f"15x{number}={result}")
-    if number == 10:
-        break
-    number+=1
--- PA — Programação e Algoritmos/Atividades/08-pratica-repeticao/4---500-decrescente-até-10.py
+++
@@ -1,7 +0,0 @@
-num=500
-
-while True:
-    print(f"{num}")
-    num-=1
-    if num == 9:
-        break
