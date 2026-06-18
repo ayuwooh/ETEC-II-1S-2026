59219d42c62476cd8f62861861ef8fb0ade5f457
ayuwooh <ayuwooh@gmail.com>
2026-06-18 18:52:02 -0300

    new py atv

---
+++ pa_programação_e_algoritmos/Atividades/atv_09_funcoes/codigos/06.py
@@ -0,0 +1,5 @@
+from functions import age_category
+
+age = int(input("Digite a idade: "))
+
+print(age_category(age))
--- pa_programação_e_algoritmos/Atividades/atv_09_funcoes/codigos/functions.py
+++ pa_programação_e_algoritmos/Atividades/atv_09_funcoes/codigos/functions.py
@@ -23,3 +23,43 @@ def find_max(var):
         if i > biggest:
             biggest = i
     return biggest
+
+
+def get_installment_rate(var):
+    if var == 1:
+        installment = -5.0  # %
+    elif var == 2:
+        installment = 1.0  # %
+    elif var == 3:
+        installment = 4.5  # %
+    elif var == 4:
+        installment = 7.5  # %
+    else:
+        installment = 10.0  # %
+    return installment
+
+
+def calc_percentage(amount, percentage):
+    value = (amount * percentage) / 100
+    return value
+
+
+def calc_grade(grade1, grade2, grade3, grade_type):
+    if grade_type == "A":
+        grade = (grade1 + grade2 + grade3) / 3
+    elif grade_type == "P":
+        grade = ((grade1 * 5) + (grade2 * 3) + (grade3 * 2)) / 10
+    return grade
+
+
+def age_category(age):
+    if age >= 5 and age <= 7:
+        return "Infantil A"
+    elif age < 10:
+        return "Infantil B"
+    elif age < 13:
+        return "Juvenil A"
+    elif age < 17:
+        return "Juvenil B"
+    else:
+        return "Adulto"
